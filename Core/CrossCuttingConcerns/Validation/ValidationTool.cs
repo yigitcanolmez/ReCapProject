b86ebcd772752273bfeabecadd049d71daaff888
Yiğitcan Ölmez <90285509+yigitcanolmez@users.noreply.github.com>
2023-01-30 11:22:07 +0300

    AOP/VALIDATION/AUTOFAC modules Added

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            #region Validation
+            //Validation nesneyle alakalıdır.
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ WebApplication1/Controllers/CarsController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly ICarService _carService;
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        [HttpGet("/getcardetail")]
+        public IActionResult GetCarDetail()
+        {
+            var result = _carService.GetCarDetail();
+            if (result.Success ==true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
